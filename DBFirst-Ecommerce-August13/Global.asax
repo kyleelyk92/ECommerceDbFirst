0344958d99e7c774f5b5a212c202bc146c3c1124
Kyle Elyk <kyleelyk92@hotmail.com>
2019-08-13 11:27:31 -0500

    Add project files.

---
+++ DBFirst-Ecommerce-August13/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DBFirst_Ecommerce_August13.MvcApplication" Language="C#" %>
---
+++ DBFirst-Ecommerce-August13/Models/CustomerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DBFirst_Ecommerce_August13.Models
+{
+    public class CustomerManager
+    {
+        ECommercePracticeAug13Entities db = new ECommercePracticeAug13Entities();
+        public ICollection<Customer> getCustomers(string city)
+        {
+            var custList = db.Customers.Where(c => c.City == city).ToList();
+
+            return custList;
+        }
+    }
+}
